ce8f25c0df994a5897789c04e4e39095d0de0e7f
Frantisek Petruv <f.petruv@gmail.com>
2023-02-08 13:10:44 -0500

    QC# Please enter the commit message for your changes. Lines starting

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,17 +1,33 @@
 <template>
   <div>hi</div>
-  <!-- <div v-for="thread in threads">{{ categories.title }}</div> -->
-  <div>{{ sourceData }}</div>
+  <div v-for="thread in threads" :key="thread.id">
+    <h2>{{ thread.title }}</h2>
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{ postByID(postId).text }}</p>
+      <p>
+        {{ userByPost(postByID(postId).userId).name }}
+      </p>
+    </div>
+  </div>
 </template>
 <script>
 import sourceData from "@/data.json";
 export default {
   data() {
     return {
-      sourceData,
-      //   threads: sourceData.categories,
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users,
     };
+  },
+  methods: {
+    postByID(postId) {
+      return this.posts.find((p) => p.id === postId);
+    },
+    userByPost(userID) {
+      return this.users.find((p) => p.id === userID);
+    },
   },
 };
 </script>
-<style scoped></style>
+<style></style>
